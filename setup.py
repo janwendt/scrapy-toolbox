47e0d0f04fb27a9dcc807c6938971529d4b87d5b
Jan Wendt <jan.wendt@online.de>
2020-10-15 14:28:01 +0200

    Adds ErrorSavingMiddleware
    Adds ErrorSavingMiddleware2
    
    abc

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scrapy-toolbox",
+    version="0.0.1",
+    author="Jan Wendt",
+    description="Saves Scrapy exceptions in your Database",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/janwendt/scrapy-toolbox",
+    download_url="https://github.com/janwendt/scrapy-toolbox/archive/0.0.1.tar.gz",
+    packages=setuptools.find_packages(),
+    install_requires=[
+          'sqlalchemy',
+          'scrapy',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
