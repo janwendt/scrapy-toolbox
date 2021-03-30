5ca81e656eb2b1829969b048894e0c1c5395cb0c
Simon Perschel <simon.perschel@t-online.de>
2021-03-30 10:04:45 +0200

    initial commit

---
+++ scrapy_toolbox/mapper.py
@@ -0,0 +1,38 @@
+import inspect
+# Get all classes from a *.py-script
+
+class ItemsModelMapper:
+
+    # For each Item there has to be a corrisponding databaseobject that extends scrapy_toolbox.database.DeclarativeBase
+    # The naming must be XYItem for Item and XY for databaseobject
+    # The item must have a ids variable with all the names of primary-keys to filter or empty list
+
+    def __init__(self, items, model):
+        self.items = items
+        self.model = model
+
+
+        self.items_col = {cls_name:cls_obj for cls_name, cls_obj in                    # "XY" : XY.__class__
+                          inspect.getmembers(self.items) if inspect.isclass(cls_obj)}
+
+        self.model_col = {cls_name + "Item" : cls_obj for cls_name, cls_obj in
+                          inspect.getmembers(self.model) if inspect.isclass(cls_obj)}  # "XYItem" : XY.__class_
+
+
+    def map_to_model(self, item, sess):
+        item_class = item.__class__
+        ids = item.ids
+
+        if len(ids) == 0:
+            model_class = self.model_col[item_class.__name__]
+            item = model_class(**{i:item[i] for i in item})
+            return item
+
+        filter_param = {item_id:item[item_id] for item_id in ids}
+        model_class = self.model_col[item_class.__name__]
+        item_by_id = sess.query(model_class).filter_by(**filter_param).first()
+        if item_by_id is None:
+            item = model_class(**{i:item[i] for i in item})
+        else:
+            item = item_by_id
+        return item
