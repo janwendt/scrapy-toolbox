a4704f6b6346c1f4c9a1bbe9f3e581903ea91ef5
Jan Wendt <janwendt@users.noreply.github.com>
2025-11-06 15:16:48 +0100

    Update middlewares.py

--- example/family/middlewares.py
+++ example/family/middlewares.py
@@ -43,7 +43,7 @@ class FamilySpiderMiddleware:
         # Should return either None or an iterable of Request or item objects.
         pass
 
-    def process_start_requests(self, start_requests, spider):
+    def process_start(self, start_requests, spider):
         # Called with the start requests of the spider, and works
         # similarly to the process_spider_output() method, except
         # that it doesn’t have a response associated.
